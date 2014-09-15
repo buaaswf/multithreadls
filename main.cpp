d09bf2042c383dc3b055330430e64c58a77bb5c2
swf0615 <876267233@qq.com>
2014-09-15 21:44:06 +0800

    aaaaaaa
    files

--- main.cpp
+++ main.cpp
@@ -17,14 +17,14 @@ int main(int argc,char **argv)
 	
 	vector<SegColonParameter>parms; parms.resize(threadnum);
 	vector<pthread_t>threads; threads.resize(threadnum);
-	for (iterFile2 = files2.begin()+1; iterFile2 != files2.end(); iterFile2++)
+	for (iterFile2 = files2.begin()+1; iterFile2 != files2.end()-4; iterFile2++)
 	{
-
+		MultiPthread *multi = new MultiPthread(threadnum, parms, threads);
 		for (size_t i = 0; i < threadnum; i++, iterFile2++)
 		{
 			iterFile2->assign(iterFile2->substr(dir2.size() + 1));
 			cout << *iterFile2 << endl;
-			MultiPthread *multi = new MultiPthread(threadnum, parms, threads);
+			
 			multi->Createthreadi(i, *iterFile2);
 			i++;
 		   
@@ -32,7 +32,7 @@ int main(int argc,char **argv)
 
 		for (int i = 0; i<threadnum; i++)
 		{
-			pthread_join(threads[i], NULL);
+			pthread_join(multi->threads[i], NULL);
 		}																			
 		
 	}
